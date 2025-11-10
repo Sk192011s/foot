947ffb89043580184dc0e1589d798ee1d035e850
Sk192011s <soekyawwin68@gmail.com>
2025-11-11 04:07:08 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
 
 Deno.serve((req) => {
   return serveDir(req, {
-    fsRoot: "public",
+    fsRoot: ".",   // ✅ root folder serve
     urlRoot: "",
     showDirListing: false,
     enableCors: true,
